80de4b0a15e1a0d0a5eb1dbafc460840f1044c47
Logan Thompson <loganthompson2011@gmail.com>
2021-11-15 17:34:02 -0500

    Fog plane and edge plane maintain a constant distance from the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,14 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private CharacterController controller;
     [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject fogPlane;
+    [SerializeField] private GameObject edgePlane;
 
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float fallSpeed = 3f;
-    [SerializeField] private float playerOffset = 19f;
+    [SerializeField] private float cameraOffset = 19f;
+    [SerializeField] private float fogPlaneOffset = 80f;
+    [SerializeField] private float edgePlaneOffset = 895f;
 
     //[SerializeField] private float moveX = 0f;
     //[SerializeField] private float moveY = 0f;
@@ -24,8 +28,12 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * Time.deltaTime * moveSpeed);
 
-        Vector3 cameraPos = new Vector3(0, 0, controller.transform.position.z - playerOffset);
+        Vector3 cameraPos = new Vector3(0, 0, controller.transform.position.z - cameraOffset);
+        Vector3 fogPos = new Vector3(0, 0, controller.transform.position.z + fogPlaneOffset);
+        Vector3 edgePos = new Vector3(0, 0, controller.transform.position.z + edgePlaneOffset);
 
         mainCamera.transform.position = cameraPos;
+        fogPlane.transform.position = fogPos;
+        edgePlane.transform.position = edgePos;
     }
 }
